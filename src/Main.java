6db7a6b7015d463735d2ca698e9cdb11128e490e
arxip <arxipov1989@gmail.com>
2023-01-29 18:40:52 +0300

    Домашка методы

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1");
         printYear(2023);
+
+        System.out.println("Задание 2");
+        printPhone(0, 2022);
+
+        System.out.println("Задание 3");
+        deliversCard(200);
 
 
     }
@@ -17,7 +26,44 @@ public class Main {
 
     public static boolean isYears(int years) {
         return years % 4 == 0 && years % 100 != 0 || years % 400 == 0;
+        //alt + shift + enter когда помогает?? missing return statement
+    }
+
+    public static void printPhone(int phone, int version) {
+        int currentYear = LocalDate.now().getYear(); //LocalDate.now() был тоже красным вроде опять alt + shift + enter
+        if (version < currentYear) {
+            System.out.println("Установите облегченную версию приложения для ");
+        } else {
+            System.out.println("Установите обычную версию приложения для ");
+        }
+        if (phone == 0) {
+            System.out.println("iOS по ссылке");
+        } else {
+            System.out.println("Android по ссылке");
         }
 
+    }
 
+    public static void deliversCard(int deliveryDistance) {
+        int days = calculateDistance(deliveryDistance);
+        if (days ==-1){
+            System.out.println("Доставки нет");
+        }else {
+            System.out.println(days + " дней потребуется на доставку");
+
+        }
+
+    }
+
+    public static int calculateDistance(int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            return 1;
+        } else if (deliveryDistance < 60) {
+            return 2;
+        } else if (deliveryDistance < 100) {
+            return 3;
+        } else { return -1;
+        }
+
+    }
 }
